04e04bca52af49f792fa5605d33a2540e7b5155a
JacobAguirre9 <jaguirre31@gatech.edu>
2022-05-15 00:27:06 -0400

    updated

--- main.py
+++ main.py
@@ -4,3 +4,11 @@
 
 print("Hello World! :)")
 
+# First, use pip install pymdptoolbox[LP] to install https://github.com/sawcordwell/pymdptoolbox
+
+import mdptoolbox, mdptoolbox.example
+P, R = mdptoolbox.example.rand(10, 3)
+pi = mdptoolbox.mdp.PolicyIteration(P, R, 0.9)
+pi.run()
+
+print(pi)
